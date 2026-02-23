dae2a2175abc582fec0f1f5bb79f7e34a5e2b285
jann3lle <jannellejofrin@gmail.com>
2026-02-23 17:55:38 +0000

    Modified files to print roll/pitch values via main.cpp

---
+++ firmware/include/sensors.h
@@ -0,0 +1,8 @@
+#pragma once
+
+typedef struct {
+    float roll;
+    float pitch;
+    float gx, gy, gz;
+    float mx, my, mz;
+} IMUData;
--- firmware/src/sensors/imu.cpp
+++ firmware/src/sensors/imu.cpp
@@ -1,3 +1,4 @@
+#include "imu.h"
 #include <Wire.h>
 #include <math.h>
 #include "ICM_20948.h" 
@@ -10,7 +11,7 @@
 
 ICM_20948_I2C myICM;
 
-void setup() {
+void imu_init() {
   Serial.begin(115200);
   delay(100);
 
@@ -36,7 +37,9 @@ void setup() {
   Serial.println("Streaming Acc (m/s^2), Roll/Pitch (deg), Gyro (dps), Mag (uT)");
 }
 
-void loop() {
+IMUData imu_read() {
+  IMUData data = {0};
+
   if (myICM.dataReady()) {
     myICM.getAGMT(); // updates accel/gyro/mag/temp
 
@@ -48,51 +51,16 @@ void loop() {
     float ay = (myICM.accY() / 1000.0f) * g;
     float az = (myICM.accZ() / 1000.0f) * g;
 
-    // Roll & pitch from accelerometer (degrees)
-    float roll  = atan2f(ay, az) * 180.0f / PI;
-    float pitch = atan2f(-ax, sqrtf(ay * ay + az * az)) * 180.0f / PI;
+    data.roll  = atan2f(ay, az) * 180.0f / PI;
+    data.pitch = atan2f(-ax, sqrtf(ay*ay + az*az)) * 180.0f / PI;
+    data.gx = myICM.gyrX();
+    data.gy = myICM.gyrY();
+    data.gz = myICM.gyrZ();
+    data.mx = myICM.magX();
+    data.my = myICM.magY();
+    data.mz = myICM.magZ();
+  } 
 
-    // Gyro in degrees/sec (dps)
-    float gx = myICM.gyrX();
-    float gy = myICM.gyrY();
-    float gz = myICM.gyrZ();
+  return data;
 
-    // Magnetometer in microtesla (uT)
-    float mx = myICM.magX();
-    float my = myICM.magY();
-    float mz = myICM.magZ();
-
-    Serial.print("Accel (m/s^2) X:");
-    Serial.print(ax, 2);
-    Serial.print(" Y:");
-    Serial.print(ay, 2);
-    Serial.print(" Z:");
-    Serial.print(az, 2);
-
-    Serial.print(" | Roll:");
-    Serial.print(roll, 2);
-    Serial.print(" deg Pitch:");
-    Serial.print(pitch, 2);
-    Serial.print(" deg");
-
-    Serial.print(" | Gyro (dps) X:");
-    Serial.print(gx, 2);
-    Serial.print(" Y:");
-    Serial.print(gy, 2);
-    Serial.print(" Z:");
-    Serial.print(gz, 2);
-
-    Serial.print(" | Mag (uT) X:");
-    Serial.print(mx, 2);
-    Serial.print(" Y:");
-    Serial.print(my, 2);
-    Serial.print(" Z:");
-    Serial.print(mz, 2);
-
-    Serial.println();
-    delay(200);
-  } else {
-    // small pause so we don't spam the CPU if data isn't ready yet
-    delay(5);
-  }
 }
