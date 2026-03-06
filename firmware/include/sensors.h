09717fe07783d8ddb2b5801602a0e753667297ed
jann3lle <jannellejofrin@gmail.com>
2026-03-06 12:38:42 +0000

    Updated sensors.h file

--- firmware/include/sensors.h
+++ firmware/include/sensors.h
@@ -6,7 +6,6 @@ typedef struct {
     float yaw;
     float gx, gy, gz;
     float mx, my, mz;
-    uint16_t fsr;    // FSR analog value (0-4095). Reads 0 until sensor is wired.
 } IMUData;
 
 typedef struct {
